3f9516e367ab98f8267175f549142b4f0a8c23a7
CaePolit <caepolit@gmail.com>
2024-06-17 14:14:32 +0200

    Ajoutez des fichiers projet.

---
+++ jeu_video/Character.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jeu_video;
+    class Character
+    {
+        public string characterName;
+        public int characterPv;
+        public int characterAtk;
+        public int characterDef;
+
+        public bool IsAlive()
+        {
+            if (characterPv <= 0) return false;
+            else return true;
+        }
+
+        public void Attack(Character foe)
+        {
+        Console.WriteLine(characterName + " attack " + foe.characterName);
+        if (this.characterAtk < foe.characterDef) { Console.WriteLine(this.characterName + " is too weak to give damage"); }
+        else { foe.characterPv = foe.characterPv - (this.characterAtk - foe.characterDef); }
+        
+        }
+    }
---
+++ jeu_video/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace jeu_video;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        //initialisation des personages 
+        Character character1 = new Character();
+        character1.characterName = "Ryu";
+        character1.characterPv = 100;
+        character1.characterAtk = 15;
+        character1.characterDef = 7;
+
+        Character character2 = new Character();
+        character2.characterName = "Ken";
+        character2.characterPv = 100;
+        character2.characterAtk = 10;
+        character2.characterDef = 13;
+
+        //ici se deroule le combat
+        while (character1.IsAlive() && character2.IsAlive())
+        {
+            Console.WriteLine(character1.characterName+": "+character1.characterPv+" VS "+character2.characterName+": "+character2.characterPv);
+            //personage1 attaque personage2
+            Console.WriteLine(character1.characterName+" atttaque "+character2.characterName);
+            character1.Attack(character2);
+            //personage2 attaque personage2
+            Console.WriteLine(character2.characterName + " repond à " + character1.characterName);
+            character2.Attack(character1);
+
+            Console.WriteLine("Tour suivant ...");
+            Console.ReadLine();
+
+        }
+        //personage1 ou personage2 est mort voire les deux
+        if (character1.IsAlive()) { Console.WriteLine(character1.characterName + " a gagné!");}
+        else { Console.WriteLine(character2.characterName + " a gagné!"); }
+
+    }
+}
